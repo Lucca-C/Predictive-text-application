fb5364cf868c8634542a4ef6e80cd45ab859f5b6
Lucca-C <luccaocamargo@gmail.com>
2022-03-28 20:01:02 +0100

    docs: added TrieNode javadocs.

--- src/TrieNode.java
+++ src/TrieNode.java
@@ -8,15 +8,35 @@ public class TrieNode {
     private boolean isWord;
 
     //-----CONSTRUCTORS-----
+
+    /**
+     * Constructor that is called recursively .
+     * @param c
+     */
     public TrieNode(char c) {
         this.c = c;
         children = new HashMap<>();
     }
+
+    /**
+     * Constructor that is called once to make the root.
+     */
     public TrieNode() {
         children = new HashMap<>();
     }
 
     //-----METHODS-----
+    /**
+     * Saves first letter of string.
+     * Checks if said letter is in the node's hashmap.
+     * If not, make a new node and save it in the hashmap,
+     * with the letter as the key.
+     * If yes, and if the word is not 1 character long,
+     * recursively call this method for the child.
+     * If it is 1 character long,
+     * Set "isWord" to true.
+     * @param word
+     */
     public void insert(String word) {
         if (word == null || word.isEmpty())
             return;
