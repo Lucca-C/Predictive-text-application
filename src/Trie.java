5dd351f12528dedc0135b8493bacd6b80644c85f
Lucca-C <luccaocamargo@gmail.com>
2022-03-29 17:57:07 +0100

    feat: split trie into predictiveText class.

--- src/Trie.java
+++ src/Trie.java
@@ -17,53 +17,6 @@ public class Trie {
             root.insert(word);
     }
 
-    //-----METHODS-----
-
-    /**
-     * Prefix is target.
-     * For every letter in target,
-     * get the node paired with that letter, in the children hashmap.
-     * Make that node ^ the current node.
-     * @param prefix
-     * @param exact
-     * @return boolean !exact or true, if lastNode is a word.
-     */
-    private boolean find(String prefix, boolean exact) {
-        TrieNode lastNode = root;
-        for (char c : prefix.toCharArray()) {
-            lastNode = lastNode.getChildren().get(c);
-            if (lastNode == null)
-                return false;
-        }
-        return !exact || lastNode.isWord();
-    }
-    private boolean find(String prefix) {
-        return find(prefix, false);
-    }
-    private void helper(TrieNode root, List<String> list, StringBuffer curr) {
-        if (root.isWord()) {
-            list.add(curr.toString());
-        }
-        if (root.getChildren() == null || root.getChildren().isEmpty())
-            return;
-        for (TrieNode child : root.getChildren().values()) {
-            helper(child, list, curr.append(child.getC()));
-            curr.setLength(curr.length() - 1);
-        }
-    }
-    public List<String> prediction(String prefix) {
-        List<String> list = new ArrayList<>();
-        TrieNode lastNode = root;
-        StringBuffer curr = new StringBuffer();
-        for (char c : prefix.toCharArray()) {
-            lastNode = lastNode.getChildren().get(c);
-            if (lastNode == null)
-                return list;
-            curr.append(c);
-        }
-        helper(lastNode, list, curr);
-        return list;
-    }
     //-----ACCESSORS & MUTATORS
     public TrieNode getRoot() {
         return root;
