959402ab23dd82fb1bc6a98de887b03f83e3397c
Lucca-C <luccaocamargo@gmail.com>
2022-03-29 17:57:21 +0100

    feat: created method to test predictive text

--- src/Tester.java
+++ src/Tester.java
@@ -3,16 +3,13 @@ import java.util.List;
 
 public class Tester {
     public static void main(String[] args){
-        FileReadWrite FileReadWrite = new FileReadWrite();
-        FileReadWrite.FileWrite("Jessio", "src/TextFiles/words.txt");
-//        FileReadWrite.FileRead("src/TextFiles/words.txt");
+        testPrediction();
+    }
 
-//        ArrayList<String> stick;
-//        stick=FileReadWrite.FileToArray("src/TextFiles/words.txt");
-//        System.out.println(stick);
-
-//        Dictionary dictionary = new Dictionary();
-//        Trie trie = new Trie(dictionary.getWords());
-//        System.out.println(trie.prediction("po"));
+    public static void testPrediction(){
+        Dictionary d = new Dictionary();
+        Trie t = new Trie(d.getWords());
+        PredictiveText p = new PredictiveText(t);
+        System.out.println(p.prediction("p"));
     }
 }
