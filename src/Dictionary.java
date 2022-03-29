caef8235064ab90c04633c37c3c7935dcacefc77
FinnbarHome <2481383@dundee.ac.uk>
2022-03-30 00:07:41 +0100

    Updated dictionaries
    Fixed spelling of dictionaries
    Put dictionaries in separate package
    Added all dictionaries as arrayLists THIS SHOULD BE CHANGED IN FUTURE TO ONLY LOAD WHEN SELECTED THE SPECIFIC LANGUAGE

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -5,7 +5,11 @@ import java.util.List;
 public class Dictionary {
     //-----FIELDS-----
    private FileReadWrite fRW = new FileReadWrite();
-   private ArrayList<String> words = fRW.FileToArray("src/TextFiles/words.txt");
+   private ArrayList<String> words = fRW.FileToArray("src/TextFiles/Dictionaries/en.txt");
+   private ArrayList<String> wordsEN2 = fRW.FileToArray("src/TextFiles/Dictionaries/en2.txt");
+   private ArrayList<String> wordsDE = fRW.FileToArray("src/TextFiles/Dictionaries/de.txt");
+   private ArrayList<String> wordsES = fRW.FileToArray("src/TextFiles/Dictionaries/es.txt");
+   private ArrayList<String> wordsFR = fRW.FileToArray("src/TextFiles/Dictionaries/fr.txt");
 //   private  List<String> words = List.of("w", "hello", "dog", "hell", "cat", "a", "hel","help","helps","helping",
 //            "potato", "porr", "wack");
 
