660bf125dcd438806b009c21ddda937e3474ab71
FinnbarHome <2481383@dundee.ac.uk>
2022-03-30 00:14:34 +0100

    Updated dictionaries
    Commented them out, was too slow atm

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -6,10 +6,10 @@ public class Dictionary {
     //-----FIELDS-----
    private FileReadWrite fRW = new FileReadWrite();
    private ArrayList<String> words = fRW.FileToArray("src/TextFiles/Dictionaries/en.txt");
-   private ArrayList<String> wordsEN2 = fRW.FileToArray("src/TextFiles/Dictionaries/en2.txt");
-   private ArrayList<String> wordsDE = fRW.FileToArray("src/TextFiles/Dictionaries/de.txt");
-   private ArrayList<String> wordsES = fRW.FileToArray("src/TextFiles/Dictionaries/es.txt");
-   private ArrayList<String> wordsFR = fRW.FileToArray("src/TextFiles/Dictionaries/fr.txt");
+//   private ArrayList<String> wordsEN2 = fRW.FileToArray("src/TextFiles/Dictionaries/en2.txt");
+//   private ArrayList<String> wordsDE = fRW.FileToArray("src/TextFiles/Dictionaries/de.txt");
+//   private ArrayList<String> wordsES = fRW.FileToArray("src/TextFiles/Dictionaries/es.txt");
+//   private ArrayList<String> wordsFR = fRW.FileToArray("src/TextFiles/Dictionaries/fr.txt");
 //   private  List<String> words = List.of("w", "hello", "dog", "hell", "cat", "a", "hel","help","helps","helping",
 //            "potato", "porr", "wack");
 
