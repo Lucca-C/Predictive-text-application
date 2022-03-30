c29e8424ec11f84b9d1aa35c766b7305cbd2665e
Lucca-C <luccaocamargo@gmail.com>
2022-03-30 18:19:51 +0100

    feat: made dictionary selection method a constructor instead.

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -6,18 +6,15 @@ public class Dictionary {
     private ArrayList<String> words;
 
     //-----CONSTRUCTORS-----
-
-    //-----METHODS-----
     /**
      * Method to select which dictionary is being utilised.
      *
      * @param selection Correlates to a numbered dictionary and is passed through from the gui
      * @param filePath  The file path of the custom dictionary
      */
-    public void selectDictionary(int selection, String filePath) {
+    public Dictionary(int selection) {
         //Default file path (so it doesn't throw errors
-        filePath = "src/TextFiles/Dictionaries/en.txt";
-
+        String filePath = "src/TextFiles/Dictionaries/en.txt";
         if (1 == selection) {
             words = fRW.FileToArray("src/TextFiles/Dictionaries/en.txt");
         } else if (2 == selection) {
@@ -31,11 +28,11 @@ public class Dictionary {
         } else if (6 == selection) {
             words = fRW.FileToArray(filePath);
         } else {
-            System.err.println("This dictionary does not exist defaulting to English");
+            System.err.println("This dictionary does not exist. Defaulting to English");
             words = fRW.FileToArray("src/TextFiles/Dictionaries/en.txt");
         }
     }
-
+    //-----METHODS-----
     /**
      * Method to print all the words in the "words" array
      */
