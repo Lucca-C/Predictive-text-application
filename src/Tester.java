1c982d08c7c9996ef5746146e99a4f74abac378f
Lucca-C <luccaocamargo@gmail.com>
2022-03-30 18:29:24 +0100

    feat: added GUI test method

--- src/Tester.java
+++ src/Tester.java
@@ -3,13 +3,19 @@ import java.util.List;
 
 public class Tester {
     public static void main(String[] args){
-        testPrediction();
+        testGUI();
     }
 
     public static void testPrediction(){
-        Dictionary d = new Dictionary();
+        Dictionary d = new Dictionary(1);
         Trie t = new Trie(d.getWords());
         PredictiveText p = new PredictiveText(t);
         System.out.println(p.prediction("p"));
+    }
+    public static void testGUI(){
+        GUI gui = new GUI();
+        gui.createMainMenu();
+        gui.createDictionary();
+        gui.createOptions();
     }
 }
