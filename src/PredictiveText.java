632da06cdafe5e547dded77e42814fe1fcf82cb4
Lucca-C <luccaocamargo@gmail.com>
2022-03-30 18:55:16 +0100

    fix: added new method and reordered methods

--- src/PredictiveText.java
+++ src/PredictiveText.java
@@ -12,6 +12,20 @@ public class PredictiveText {
     }
 
     //-----METHODS-----
+    public List<String> prediction(String target) {
+        List<String> list = new ArrayList<>();
+        TrieNode lastNode = trie.getRoot();
+        StringBuffer current = new StringBuffer();
+        for (char c : target.toCharArray()) {
+            lastNode = lastNode.getChildren().get(c);
+            if (lastNode == null)
+                return list;
+            current.append(c);
+        }
+        helper(lastNode, list, current);
+        return list;
+    }
+
     private boolean find(String target, boolean specific) {
         TrieNode lastNode = trie.getRoot();
         for (char c : target.toCharArray()) {
@@ -21,29 +35,21 @@ public class PredictiveText {
         }
         return !specific || lastNode.isWord();
     }
-    private void helper(TrieNode root, List<String> list, StringBuffer curr) {
+
+    public boolean find(String target) {
+        return find(target, false);
+    }
+
+    private void helper(TrieNode root, List<String> list, StringBuffer current) {
         if (root.isWord()) {
-            list.add(curr.toString());
+            list.add(current.toString());
         }
         if (root.getChildren() == null || root.getChildren().isEmpty())
             return;
         for (TrieNode child : root.getChildren().values()) {
-            helper(child, list, curr.append(child.getC()));
-            curr.setLength(curr.length() - 1);
+            helper(child, list, current.append(child.getC()));
+            current.setLength(current.length() - 1);
         }
-    }
-    public List<String> prediction(String target) {
-        List<String> list = new ArrayList<>();
-        TrieNode lastNode = trie.getRoot();
-        StringBuffer curr = new StringBuffer();
-        for (char c : target.toCharArray()) {
-            lastNode = lastNode.getChildren().get(c);
-            if (lastNode == null)
-                return list;
-            curr.append(c);
-        }
-        helper(lastNode, list, curr);
-        return list;
     }
     //-----ACCESSORS & MUTATORS-----
     public Trie getTrie() {
